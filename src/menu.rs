d6cb6d416595a23c99402a4c708a1e330a0abb09
alannxq <85573908+alannxq@users.noreply.github.com>
2022-06-10 10:48:40 +0100

    Update menu.rs

--- src/menu.rs
+++ src/menu.rs
@@ -16,7 +16,7 @@ enum menu_option {
 }
 
 pub fn display_menu() -> bool {
-	let mut current_selection = menu_option::Start;
+	let mut current_selection = menu_option::Start; // by default, "Start" will be selected first
 	let reader = Reader;
 
 	loop {
@@ -26,6 +26,7 @@ pub fn display_menu() -> bool {
 			menu_option::Quit => println!("   Start Game\n-> Quit Game")
 		}
 		let event = reader.read_key().expect("Could not read key");
+		// change selected option with arrow keys, break loop with ENTER
 		match event {
 			KeyEvent {
 				code: KeyCode::Up,
@@ -42,6 +43,7 @@ pub fn display_menu() -> bool {
 			_ => {}
 		}
 	}
+	// depending on the selection before breaking with ENTER, either return true or false
 	match current_selection {
 		menu_option::Start => true,
 		menu_option::Quit => false
